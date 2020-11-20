b04f36d0346ee942562b53cf9aee7476a5d18a77
ken <ken@MK.local>
2020-11-20 00:18:20 -0800

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
-
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    }, 
+    },
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
@@ -26,15 +26,41 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
             }}>
                 <AppNavigator />
             </View>
@@ -43,34 +69,3 @@ class Main extends Component {
 }
 
 export default Main;
-
-// class Main extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             campsites: CAMPSITES,
-//             selectedCampsite: null
-//         };
-//     }
-
-//     onCampsiteSelect(campsiteId) {
-//         this.setState({selectedCampsite: campsiteId});
-//     }
-    
-//     render() {
-//         return (
-//             <View style={{flex: 1}}>
-//                 <Directory
-//                     campsites={this.state.campsites}
-//                     onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-//                 />
-//                 <CampsiteInfo
-//                     campsite={this.state.campsites.filter(
-//                         campsite => campsite.id === this.state.selectedCampsite)[0]}
-//                 />
-//             </View>
-//         );
-//     }
-// }
-
-// export default Main;
